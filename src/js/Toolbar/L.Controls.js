2e2b11e528521afd838d2ea88dc27b8076051c28
Gabriel <74922336+sheeley820@users.noreply.github.com>
2024-05-08 16:40:45 -0400

    Code cleanup

--- src/js/Toolbar/L.Controls.js
+++ src/js/Toolbar/L.Controls.js
@@ -169,6 +169,7 @@ const PMButton = L.Control.extend({
             callback: (event, action) => {
               const colorIcon = head(action.children);
               colorIcon.style.backgroundColor = event.activeColor;
+              console.log('Color should be changing...', colorIcon);
             },
           },
         ],
