104a38195221116f125819518704188e2602241b
Gabriel <74922336+sheeley820@users.noreply.github.com>
2024-05-09 15:41:28 -0400

    Fix a couple of bugs

--- src/js/L.PM.Map.js
+++ src/js/L.PM.Map.js
@@ -145,9 +145,6 @@ const Map = L.Class.extend({
       ignoreShapes: ['Text'],
       merge: true,
     });
-    if (options.activeColor) {
-      this._fireColorChanged(options.activeColor, 'Global');
-    }
   },
   setGlobalOptions(o) {
     // merge passed and existing options
