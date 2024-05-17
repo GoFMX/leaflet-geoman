de117a0cd95991248da8625a2563df4e486db325
Gabriel <74922336+sheeley820@users.noreply.github.com>
2024-05-17 17:35:15 -0400

    Add the color prop to the layers

--- dist/leaflet-geoman.js
+++ dist/leaflet-geoman.js
@@ -18205,6 +18205,7 @@
         snapMiddle: !1,
         allowSelfIntersection: !0,
         tooltips: !0,
+        color: '#3388ff',
         templineStyle: {},
         hintlineStyle: { color: '#3388ff', dashArray: '5,5' },
         pathOptions: null,
